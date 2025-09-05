dd8909b9287db7b554fbd5ea9c31eabd8c7a9742
Kaylazagelbaum <kzagelba@student.touro.edu>
2025-09-05 16:39:53 -0400

    Add files via upload

---
+++ Module1_Kayla_Zagelbaum.py
@@ -0,0 +1,18 @@
+Python 3.13.7 (v3.13.7:bcee1c32211, Aug 14 2025, 19:10:51) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> print ('hello world')
+hello world
+>>> def hello():
+...     print ('hello all')
+... 
+...     
+>>> hello
+<function hello at 0x107b39a80>
+>>> hello()
+hello all
+>>> def hello2 (name):
+...     print ('hello',name,"!")
+... 
+...     
+>>> hello2('MCON 141!!!')
+hello MCON 141!!! !
